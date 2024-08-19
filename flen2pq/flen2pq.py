43c44319102fc741d5573e742e24e796eb61a346
francescomagarotto <magarotto.francesco@gmail.com>
2024-08-19 21:45:41 +0200

    chunk_size logic

--- flen2pq/flen2pq.py
+++ flen2pq/flen2pq.py
@@ -7,6 +7,8 @@ import yaml
 import argparse
 import sys
 
+from pandas.errors import ParserError
+
 
 class TypeHandlers:
 
@@ -22,11 +24,12 @@ class TypeHandlers:
 
 
 class FixedWidthToParquetConverter:
-    def __init__(self, input_file, output_file, fields, chunk_size=10000):
+    def __init__(self, input_file, output_file, fields, chunk_size=10000, fault_tolerant = True):
         self.input_file = input_file
         self.output_file = output_file
         self.fields = fields
         self.chunk_size = chunk_size
+        self.fault_tolerant = fault_tolerant
 
     def convert(self):
         # Extract column names and widths
@@ -38,14 +41,12 @@ class FixedWidthToParquetConverter:
         pq_writer = None
         in_error = False
         # Read the file in chunks
-        with open(self.input_file) as file:
-            while True:
-                try:
-                    # Read a chunk of rows
-                    chunk = pd.read_fwf(file, widths=column_widths, names=column_names, nrows=self.chunk_size,
-                                        dtype=str,
-                                        keep_default_na=False)
 
+        with pd.read_fwf(self.input_file, widths=column_widths, names=column_names, chunksize=self.chunk_size,
+                         dtype=str,
+                         keep_default_na=False) as file:
+            try:
+                for chunk in file:
                     # If the chunk is empty, we have finished reading
                     if chunk.empty:
                         break
@@ -77,9 +78,13 @@ class FixedWidthToParquetConverter:
                         first_chunk = False
 
                     pq_writer.write_table(table)
-                except Exception as e:
-                    in_error = True
-                    print(e)
+            except ParserError as pe:
+                print(pe)
+                if self.fault_tolerant is False:
+                    raise pe
+            except Exception as e:
+                in_error = True
+                print(e)
 
         # Finalize and close the Parquet writer
         if pq_writer:
@@ -99,21 +104,23 @@ class ConfigLoader:
             config = yaml.safe_load(file)
             input_file = config['input']
             output_file = config['output']
+            fault_tolerant = config['fault_tolerant']
             fields = config['fields']
             chunk_size = config.get('chunk_size', 10000)
-            return input_file, output_file, fields, chunk_size
+            return input_file, output_file, fields, chunk_size, fault_tolerant
 
     @staticmethod
     def from_args(args):
         input_file = args.input_file
         output_file = args.output_file
         column_names = args.column_names.split(',')
+        fault_tolerant = args.fault_tolerant
         column_widths = list(map(int, args.column_widths.split(',')))
         column_types = args.column_types.split(',')
         fields = [{'name': name, 'length': length, 'type': dtype}
                   for name, length, dtype in zip(column_names, column_widths, column_types)]
         chunk_size = args.chunk_size
-        return input_file, output_file, fields, chunk_size
+        return input_file, output_file, fields, chunk_size, fault_tolerant
 
 
 def parse_args():
@@ -124,6 +131,7 @@ def parse_args():
     parser.add_argument('--output_file', type=str, help='Path to the output Parquet file.')
     parser.add_argument('--column_names', type=str, help='Comma-separated column names.')
     parser.add_argument('--column_widths', type=str, help='Comma-separated column widths.')
+    parser.add_argument("--fault-tolerant", type=bool, help='Fault tolerant processing')
     parser.add_argument('--column_types', type=str, help='Comma-separated column types (int, float, string).')
     parser.add_argument('--chunk_size', type=int, default=10000,
                         help='Number of rows per chunk (default: 10000).')
@@ -133,24 +141,22 @@ def parse_args():
 
     return parser.parse_args()
 
-
 def main():
     args = parse_args()
 
     if args.config_file:
         # Load parameters from the YAML file
-        input_file, output_file, fields, chunk_size = ConfigLoader.from_yaml(args.config_file)
+        input_file, output_file, fields, chunk_size, fault_tolerant = ConfigLoader.from_yaml(args.config_file)
     elif args.input_file and args.output_file and args.column_names and args.column_widths and args.column_types:
         # Load parameters from command-line arguments
-        input_file, output_file, fields, chunk_size = ConfigLoader.from_args(args)
+        input_file, output_file, fields, chunk_size, fault_tolerant = ConfigLoader.from_args(args)
     else:
         print("Error: Please specify either the YAML file or all required parameters.")
         sys.exit(1)
 
     # Create the converter object and start the conversion
-    converter = FixedWidthToParquetConverter(input_file, output_file, fields, chunk_size)
+    converter = FixedWidthToParquetConverter(input_file, output_file, fields, chunk_size, fault_tolerant)
     converter.convert()
-
 
 if __name__ == '__main__':
     main()
--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import numpy
 from setuptools import setup
 from Cython.Build import cythonize
 
@@ -5,6 +6,9 @@ setup(
     name='flen2pq',
     version='1.0.0',
     py_modules=['flen2pq'],
-    ext_modules=cythonize('flen2pq.py'),
+    ext_modules=cythonize('flen2pq/flen2pq.py', compiler_directives={"language_level": "3"}, annotate=True
+                          ),
+
+    zip_safe=False,
     long_description=open('README.rst').read(),
 )
